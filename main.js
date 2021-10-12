ddebec896af96ecab298fbbe856fc1a7b8b7475e
olegscherbak <nix.001@ukr.net>
2021-10-12 21:22:17 +0300

    hw-js-11-switch-v3

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
+const colors = [
+    "#FFFFFF",
+    "#2196F3",
+    "#4CAF50",
+    "#FF9800",
+    "#009688",
+    "#795548"
+];
+
 const refs = {
-    startBtn: document.querySelector('button[data-action="start"'),
-    stopBtn: document.querySelector('button[data-action="stop"'),
-  },
-  colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ],
-  randomIntegerFromInterval = (max) => {
-    return Math.floor(Math.random() * (max + 1));
-  },
-  randomColor = () => {
-    const color = colors[randomIntegerFromInterval(colors.length - 1)]
-    console.log(color)
-    document.body.style.backgroundColor = color
-  }
+  body: document.querySelector("body"),
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]')
+}
 
-let interval = undefined
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-refs.startBtn.addEventListener('click', element => interval = interval ?
-  interval :
-  setInterval(() => randomColor(), 1000))
+let intervalId;
 
-refs.stopBtn.addEventListener('click', element => clearInterval(interval))
+const changeColor = () => {
+  let color = randomIntegerFromInterval(0, colors.length - 1);
+  refs.body.bgColor = colors[color];
+}
+
+const startHandler = () => {
+  refs.start.disabled = true;
+  intervalId = setInterval(changeColor, 1000);
+}
+
+const stopHandler = () => {
+  clearInterval(intervalId);
+  refs.start.disabled = false;
+}
+
+refs.start.addEventListener('click', startHandler);
+refs.stop.addEventListener('click', stopHandler);
